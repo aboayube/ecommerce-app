cb4ad59fa0050448d0b75a5911cdb304158c9be3
unknown <snuora2019@gmail.com>
2022-05-29 05:27:26 +0300

    threed

--- app/Helpers/ImageClass.php
+++ app/Helpers/ImageClass.php
@@ -12,34 +12,34 @@ class ImageClass
         $filename = time() . '-' . time() . '.' . $file->getClientOriginalName();
         $path = public_path('assets/' . $foldername . '/' . $filename);
 
-        Image::make($file->getRealPath())->resize(538, 200)->save($path, 100);
+        //  Image::make($file->getRealPath())->resize(538, 200)->save($path, 100);
         return  $filename;
     }
     public static function update($file, $oldname, $foldername)
     {
-        if ($oldname) {
+        /*      if ($oldname) {
             if (File::exists('assets/' . $foldername . '/' . $oldname)) {
                 unlink('assets/' . $foldername . '/' . $oldname);
             }
-        }
+        } */
 
         $filename = time() . '-' . time() . '.' . $file->getClientOriginalExtension();
-        $path = public_path('assets/' . $foldername . '/' . $filename);
-        Image::make($file->getRealPath())->resize(800, null, function ($constraint) {
+        // $path = public_path('assets/' . $foldername . '/' . $filename);
+        /*  Image::make($file->getRealPath())->resize(800, null, function ($constraint) {
             $constraint->aspectRatio();
-        })->save($path, 100);
+        })->save($path, 100); */
 
         return $filename;
     }
 
     public static function delete($image, $folder)
     {
-        if ($image) {
+        /*    if ($image) {
 
             if (File::exists('assets/' . $folder . '/' . $image)) {
 
                 unlink('assets/' . $folder . '/' . $image);
             }
-        }
+        } */
     }
 }
